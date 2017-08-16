028ba47eb876bda337f13abcc3e0304ef571ccfa
haidev <haidev.tang@enchantin.com>
2017-08-16 15:21:54 +0800

    puas master

--- app/src/main/java/com/thd/expandabletextview/MainActivity.java
+++ app/src/main/java/com/thd/expandabletextview/MainActivity.java
@@ -1,13 +1,30 @@
 package com.thd.expandabletextview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private TextView mTv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.bt_fun2);
+        mTv1 = (TextView) findViewById(R.id.tv_des);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTv1.setText(button1.getText());
+            }
+        });
+
     }
 }
