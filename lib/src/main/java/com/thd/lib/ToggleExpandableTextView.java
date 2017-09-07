5aa9a19c3f2adc340f99c0b00d7566c57fa06294
tanghaidev <tanghaidev@163.com>
2017-09-07 10:14:10 +0800

    Update ToggleExpandableTextView.java

--- lib/src/main/java/com/thd/lib/ToggleExpandableTextView.java
+++ lib/src/main/java/com/thd/lib/ToggleExpandableTextView.java
@@ -90,7 +90,7 @@ public class ToggleExpandableTextView extends TextView implements View.OnClickLi
         mAnimationDuration = typedArray.getInt(R.styleable.ExpandableTextView_animDuration, DEFAULT_ANIM_DURATION);
         mAnimAlphaStart = typedArray.getFloat(R.styleable.ExpandableTextView_animAlphaStart, DEFAULT_ANIM_ALPHA_START);
         useToggleDefualtAnimator = typedArray.getBoolean(R.styleable.ExpandableTextView_useToggleViewDefaultAnimator, DEFAULT_USE_TOGGLEVIEW_ANIMATOR);
-
+        int a =10;
         typedArray.recycle();
 
     }
